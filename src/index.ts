4e0e1ba9aaba8073c9976629501be5f530c0f5c8
Marc de Jong <marcdejong@protonmail.com>
2025-07-01 17:49:09 +0200

    feat: unzipping of pptx file

--- src/index.ts
+++ src/index.ts
@@ -75,3 +75,30 @@ export async function pptxSweep(
   // 5. Write the updated buffer to the destination file
   fs.writeFileSync(destinationPath, updatedBuffer);
 }
+
+export async function unzip(
+  sourcePath: string,
+  destinationPath: string
+): Promise<void> {
+  const zip = new JSZip();
+  const buffer = fs.readFileSync(sourcePath);
+
+  await zip.loadAsync(buffer);
+
+  // Ensure the destination directory exists
+  fs.mkdirSync(destinationPath);
+
+  const files = Object.keys(zip.files);
+  for (const file of files) {
+    const content = await zip.file(file)?.async("nodebuffer");
+    if (content) {
+      const destFilePath = `${destinationPath}/${file}`;
+      fs.mkdirSync(
+        destFilePath.substring(0, destFilePath.lastIndexOf("/")),
+
+        { recursive: true }
+      );
+      fs.writeFileSync(destFilePath, content);
+    }
+  }
+}
